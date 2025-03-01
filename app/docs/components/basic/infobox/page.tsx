f9cd285c07c6cb3fdd629f4fd38a9f27ecc332bb
Helin Biçen <helinbicen@outlook.com>
2025-03-02 01:55:42 +0300

    Add WarningBlock Component docs

--- app/docs/components/basic/infobox/page.tsx
+++ app/docs/components/basic/infobox/page.tsx
@@ -2,7 +2,12 @@
 
 import React, { useState, useEffect } from "react";
 import { CopyBlock, dracula } from "react-code-blocks";
-import { BsAirplaneFill, BsExclamationCircleFill, BsExclamationTriangleFill, BsInfoCircleFill, BsXCircleFill } from "react-icons/bs";
+import {
+  BsAirplaneFill,
+  BsExclamationTriangleFill,
+  BsInfoCircleFill,
+  BsXCircleFill,
+} from "react-icons/bs";
 
 const InfoBoxPage: React.FC = () => {
   const [hydrated, setHydrated] = useState(false);
@@ -21,10 +26,11 @@ const InfoBoxPage: React.FC = () => {
         InfoBox Bileşeni
       </h1>
       <p className="text-gray-600 mb-6 text-lg">
-      Infobox bileşeni, kullanıcılara önemli bilgiler veya yönlendirmeler sunmak amacıyla kullanılan görsel bir bileşendir. Genellikle kısa ve öz bilgi içerir ve genellikle uyarılar, bilgilendirmeler veya hatırlatmalar için tasarlanır.
-Kullanıcı
-        deneyimini artırmak için farklı stiller ve ikon desteğiyle
-        özelleştirilebilir.
+        Infobox bileşeni, kullanıcılara önemli bilgiler veya yönlendirmeler
+        sunmak amacıyla kullanılan görsel bir bileşendir. Genellikle kısa ve öz
+        bilgi içerir ve genellikle uyarılar, bilgilendirmeler veya hatırlatmalar
+        için tasarlanır. Kullanıcı deneyimini artırmak için farklı stiller ve
+        ikon desteğiyle özelleştirilebilir.
       </p>
 
       <h2 className="text-2xl font-bold mb-4 text-gray-700">Kullanım</h2>
@@ -51,51 +57,44 @@ Kullanıcı
           {
             title: "Uyarı Kullanımı",
             code: `<InfoBox variant="warning">Uyarı</InfoBox>`,
-            infoBoxClass:
-              "border-yellow-500",
-            icon: <BsExclamationTriangleFill className="mr-[7px] text-yellow-500" />,
+            infoBoxClass: "border-yellow-500",
+            icon: (
+              <BsExclamationTriangleFill className="mr-[7px] text-yellow-500" />
+            ),
             label: "Uyarı",
           },
           {
             title: "Hata Kullanımı",
             code: `<InfoBox variant="error">Hata</InfoBox>`,
-            infoBoxClass:
-              "border-red-500",
+            infoBoxClass: "border-red-500",
             icon: <BsXCircleFill className="mr-[7px] text-red-500" />,
             label: "Hata",
           },
           {
             title: "Özel İkon Kullanımı",
             code: `<InfoBox icon={</BsAirplaneFill />}>Özel İkon</InfoBox>`,
-            infoBoxClass:
-              "border-blue-500",
+            infoBoxClass: "border-blue-500",
             icon: <BsAirplaneFill className="mr-[7px] text-blue-500" />,
             label: "Özel ikon",
-          }
-          
-        ].map(
-          (
-            { title, code, infoBoxClass, label, icon},
-            index
-          ) => (
-            <div key={index} className="bg-gray-50 p-6 rounded-lg shadow">
-              <p className="mb-3 font-semibold text-gray-700">{title}</p>
-              <CopyBlock
-                text={code}
-                language="jsx"
-                showLineNumbers
-                theme={dracula}
-                wrapLines
-              />
-              <div
-                className={`${infoBoxClass} mt-5 flex items-center py-[12px] px-[10px] border-[3.008px] border-t-0 border-r-0 border-b-0`}
-              >
-                {icon}
-                {label}
-              </div>
+          },
+        ].map(({ title, code, infoBoxClass, label, icon }, index) => (
+          <div key={index} className="bg-gray-50 p-6 rounded-lg shadow">
+            <p className="mb-3 font-semibold text-gray-700">{title}</p>
+            <CopyBlock
+              text={code}
+              language="jsx"
+              showLineNumbers
+              theme={dracula}
+              wrapLines
+            />
+            <div
+              className={`${infoBoxClass} mt-5 flex items-center py-[12px] px-[10px] border-[3.008px] border-t-0 border-r-0 border-b-0`}
+            >
+              {icon}
+              {label}
             </div>
-          )
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ app/docs/components/basic/warningblock/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { CopyBlock, dracula } from "react-code-blocks";
+import {
+  BsChevronDoubleRight,
+  BsInfoCircleFill,
+} from "react-icons/bs";
+
+const WarningBlockPage: React.FC = () => {
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  if (!hydrated) {
+    return null;
+  }
+
+  return (
+    <div className="p-6 max-w-3xl mx-auto bg-white rounded-lg shadow-md">
+      <h1 className="text-4xl font-extrabold mb-6 text-gray-800">
+        WarningBlock Bileşeni
+      </h1>
+      <p className="text-gray-600 mb-6 text-lg">
+        WarningBlock bileşeni, kullanıcılara uyarı mesajları ve bu mesajla
+        beraber bir buton gösterme işlevi ile kullanılır. Genelde detaylı bilgi
+        içeren sayfalara yönlendirmede kullanılır deneyimini artırmak için
+        farklı stiller ve ikon desteğiyle özelleştirilebilir.
+      </p>
+
+      <h2 className="text-2xl font-bold mb-4 text-gray-700">Kullanım</h2>
+      <div className="bg-gray-100 p-4 rounded-lg">
+        <CopyBlock
+          text={`<WarningBlock buttonText="Daha fazla bilgi" 
+onClick={handleClick} 
+warning="Bagaj hakları ile ilgili sorunları önlemek 
+adına bagaj düzenlemelerini inceleyiniz." />`}
+          language="jsx"
+          showLineNumbers
+          theme={dracula}
+          wrapLines
+        />
+      </div>
+
+      <h2 className="text-2xl font-bold mt-8 mb-4 text-gray-700">Örnekler</h2>
+      <div className="space-y-8">
+        {[
+          {
+            title: "Varsayılan Kullanım",
+            code: `<WarningBlock buttonText="Daha fazla bilgi" 
+onClick={handleClick} 
+warning="Bagaj hakları ile ilgili sorunları önlemek 
+adına bagaj düzenlemelerini inceleyiniz." />`,
+            warningBlockClass:
+              "flex gap-[12px] border-[1px] border-[#c3cbd7] p-[15px] px-[15.6px] text-[14px] items-center mt-[30px]",
+            warning:
+              "Bagaj hakları ile ilgili sorunları önlemek adına bagaj düzenlemelerini inceleyiniz.",
+            buttonText: "Daha fazla bilgi",
+          },
+        ].map(
+          ({ title, code, warningBlockClass, warning, buttonText }, index) => (
+            <div key={index} className="bg-gray-50 p-6 rounded-lg shadow">
+              <p className="mb-3 font-semibold text-gray-700">{title}</p>
+              <CopyBlock
+                text={code}
+                language="jsx"
+                showLineNumbers
+                theme={dracula}
+                wrapLines
+              />
+              <div className={warningBlockClass}>
+                <BsInfoCircleFill className="text-[20px] flex-shrink-0" />
+                <div className="">
+                  {warning}
+                  <button className="flex items-center gap-[8px] text-red-500 cursor-pointer hover:text-red-700">
+                    {buttonText}
+                    <BsChevronDoubleRight className="stroke-[1]" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default WarningBlockPage;
